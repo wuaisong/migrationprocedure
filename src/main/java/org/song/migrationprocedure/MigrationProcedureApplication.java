54f1758b47ff88ca783b22916fa6e27f2f2d6ee7
吴亚明 <wuyaming@yxt.com>
2022-02-25 10:14:55 +0800

    项目初始化

---
+++ src/main/java/org/song/migrationprocedure/MigrationProcedureApplication.java
@@ -0,0 +1,16 @@
+package org.song.migrationprocedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyaming
+ */
+@SpringBootApplication
+public class MigrationProcedureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationProcedureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/song/migrationprocedure/MigrationprocedureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.song.migrationprocedure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationprocedureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
